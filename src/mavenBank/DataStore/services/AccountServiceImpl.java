498e96d81238648bcb14b06fe14e6a459d670fbb
akenz1901 <michael_aka1@icloud.com>
2021-08-17 18:04:37 +0100

    BigDecimal was corrected by making use an inbuilt static method to give and initial value ZERO

--- src/mavenBank/DataStore/services/AccountServiceImpl.java
+++ src/mavenBank/DataStore/services/AccountServiceImpl.java
@@ -5,6 +5,9 @@ import mavenBank.AccountType;
 import mavenBank.Customer;
 import mavenBank.DataStore.CustomerRepo;
 import mavenBank.Exceptions.MavenBankException;
+import mavenBank.Exceptions.MavenBankTransactionException;
+
+import java.math.BigDecimal;
 
 public class AccountServiceImpl implements AccountService{
     @Override
@@ -24,6 +27,43 @@ public class AccountServiceImpl implements AccountService{
         CustomerRepo.getCustomers().put(customer.getBVN(),customer);
         return newAccount.getAccountNumber();
     }
+
+    @Override
+    public BigDecimal deposit(BigDecimal amount, long accountNumber) throws MavenBankException, MavenBankTransactionException {
+        if(amount.compareTo(BigDecimal.ZERO) < 0){
+            throw new MavenBankTransactionException("Invalid Amount");
+        }
+        BigDecimal balance = BigDecimal.ZERO;
+        Account depositAccount = findAccount(accountNumber);
+        depositAccount.getBalance().add(amount);
+        return amount;
+    }
+
+    @Override
+    public Account findAccount(Customer customer, long accountNumber)throws MavenBankException {
+
+        return null;
+    }
+
+    @Override
+    public Account findAccount(long accountNumber) throws MavenBankException{
+        boolean accountFound = false;
+        Account foundAccount = null;
+        for (Customer customer: CustomerRepo.getCustomers().values()) {
+            for (Account account:customer.getAccounts()){
+                if(account.getAccountNumber() == accountNumber){
+                    foundAccount = account;
+                    accountFound = true;
+                    break;
+                }
+                if(accountFound)
+                    break;
+            }
+
+        }
+        return foundAccount;
+        }
+
     public boolean accountTypeExists(Customer customer, AccountType typeofAccount){
 
         boolean checkIfItExist = false;
