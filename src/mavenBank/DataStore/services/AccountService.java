f25fc5415c5f8d132c8bd87a1e82c7906d54f6ad
akenz1901 <michael_aka1@icloud.com>
2021-08-17 18:00:39 +0100

    Deposit method added with severak exceptions added

--- src/mavenBank/DataStore/services/AccountService.java
+++ src/mavenBank/DataStore/services/AccountService.java
@@ -1,9 +1,16 @@
 package mavenBank.DataStore.services;
 
+import mavenBank.Account;
 import mavenBank.AccountType;
 import mavenBank.Customer;
 import mavenBank.Exceptions.MavenBankException;
 
+import java.math.BigDecimal;
+
 public interface AccountService {
     long openAccount(Customer customer, AccountType typeofAccount) throws MavenBankException;
+    BigDecimal deposit(BigDecimal decimal, long accountNumber) throws MavenBankException;
+
+    Account findAccount(Customer customer, long accountNumber) throws MavenBankException;
+    Account findAccount(long accountNumber) throws MavenBankException;
 }
