a04c214609eac46d79998aa0771c23512332471f
akenz1901 <michael_aka1@icloud.com>
2021-08-17 18:08:02 +0100

    Maven Exception Added

--- src/mavenBank/Exceptions/MavenBankTransactionException.java
+++ src/mavenBank/Exceptions/MavenBankTransactionException.java
@@ -1,2 +1,18 @@
-package mavenBank.Exceptions;public class MavenBankTransactionException {
+package mavenBank.Exceptions;
+
+public class MavenBankTransactionException extends MavenBankException{
+    public MavenBankTransactionException() {
+    }
+
+    public MavenBankTransactionException(String message) {
+        super(message);
+    }
+
+    public MavenBankTransactionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MavenBankTransactionException(Throwable cause) {
+        super(cause);
+    }
 }
