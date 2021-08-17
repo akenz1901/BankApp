bb5b3bcee69c70d978f75702cee4fd2e53d59e3b
akenz1901 <michael_aka1@icloud.com>
2021-08-17 18:02:47 +0100

    Static initializer was added Which helps us add automatic customers in the customer repo

--- src/mavenBank/DataStore/CustomerRepo.java
+++ src/mavenBank/DataStore/CustomerRepo.java
@@ -1,7 +1,11 @@
 package mavenBank.DataStore;
 
+import mavenBank.Account;
+import mavenBank.AccountType;
 import mavenBank.Customer;
+import mavenBank.DataStore.services.BankService;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,5 +18,40 @@ public class CustomerRepo {
 
     private void setCustomers(Map<Long, Customer> customers) {
         customers = customers;
+    }
+
+//    public static void tearDown(){
+//        customers=null;
+//    }
+    static {
+        setUp();
+}
+
+    public static void setUp(){
+        Customer joshua = new Customer();
+        joshua.setBVN(1);
+        joshua.setFirstName("Dome");
+        joshua.setSurname("joshua");
+        joshua.setEmail("akenz1901@gmail.com");
+        joshua.setPhone("08183563309");
+        joshua.setPassword("1248489284749");
+        Account joshuaSavingsAccount = new Account(1000110001, AccountType.SAVINGS);
+        joshua.getAccounts().add(joshuaSavingsAccount);
+
+        Account joshuaCurrentAccount = new Account(1000110002, AccountType.CURRENT, new BigDecimal(500000000));
+        joshua.getAccounts().add(joshuaCurrentAccount);
+        customers.put(joshua.getBVN(), joshua);
+
+        Customer judas = new Customer();
+        judas.setBVN(2);
+        judas.setFirstName("Dome");
+        judas.setSurname("joshua");
+        judas.setEmail("akenz1901@gmail.com");
+        judas.setPhone("08183563309");
+        judas.setPassword("1248489284749");
+        Account judasSavingsAccount = new Account(1000110003, AccountType.SAVINGS);
+        judas.getAccounts().add(judasSavingsAccount);
+        customers.put(judas.getBVN(), judas);
+
     }
 }
