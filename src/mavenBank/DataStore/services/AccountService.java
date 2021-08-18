0e2f7e85aa123a4c66f9852fab100f091642753e
akenz1901 <michael_aka1@icloud.com>
2021-08-18 22:29:41 +0100

    Instance Method was added in the interface for implementation

--- src/mavenBank/DataStore/services/AccountService.java
+++ src/mavenBank/DataStore/services/AccountService.java
@@ -1,8 +1,9 @@
 package mavenBank.DataStore.services;
 
-import mavenBank.Account;
-import mavenBank.AccountType;
-import mavenBank.Customer;
+import Entities.Account;
+import mavenBank.DataStore.AccountType;
+import Entities.Customer;
+import mavenBank.DataStore.LoanStatus;
 import mavenBank.Exceptions.MavenBankException;
 
 import java.math.BigDecimal;
@@ -11,6 +12,13 @@ public interface AccountService {
     long openAccount(Customer customer, AccountType typeofAccount) throws MavenBankException;
     BigDecimal deposit(BigDecimal decimal, long accountNumber) throws MavenBankException;
 
+    long openSavingsAccount(Customer customer)throws MavenBankException;
+    long OpenCurrentAccount(Customer customer)throws MavenBankException;
+
     Account findAccount(Customer customer, long accountNumber) throws MavenBankException;
     Account findAccount(long accountNumber) throws MavenBankException;
+
+    BigDecimal withdraw(BigDecimal decimal, long accountNumber) throws MavenBankException;
+    void applyForOverdraft(Account account);
+    LoanStatus applyForLoan();
 }
--- src/mavenBank/DataStore/services/AccountServiceImpl.java
+++ src/mavenBank/DataStore/services/AccountServiceImpl.java
@@ -1,10 +1,9 @@
 package mavenBank.DataStore.services;
 
-import Entities.Account;
-import Entities.AccountType;
-import Entities.Customer;
+import Entities.*;
+import mavenBank.DataStore.AccountType;
 import mavenBank.DataStore.CustomerRepo;
-import mavenBank.DataStore.TransactionServiceType;
+import mavenBank.DataStore.LoanStatus;
 import mavenBank.Exceptions.MavenBankException;
 import mavenBank.Exceptions.MavenBankInsufficientAmountException;
 import mavenBank.Exceptions.MavenBankTransactionException;
@@ -15,20 +14,14 @@ public class AccountServiceImpl implements AccountService{
 
     @Override
     public long openAccount(Customer customer, AccountType typeofAccount) throws MavenBankException {
-        if(customer == null || typeofAccount==null) {
-            throw new MavenBankException("Customer or Type Can't be Null");
+       long accountNumber = BigDecimal.ZERO.longValue();
+        if(typeofAccount == AccountType.SAVINGSACCOUNT){
+            accountNumber = openSavingsAccount(customer);
         }
-
-        if(accountTypeExists(customer, typeofAccount)){
-            throw new MavenBankException("Account Type already Exists for this user, Thanks");
+        else if(typeofAccount == AccountType.CURRENTACCOUNT){
+            accountNumber = OpenCurrentAccount(customer);
         }
-        Account newAccount = new Account();
-        newAccount.setTypeOfAccount(AccountType.SAVINGS);
-        newAccount.setAccountNumber(BankService.generateAccountNumber());
-        customer.getAccounts().add(newAccount);
-
-        CustomerRepo.getCustomers().put(customer.getBVN(),customer);
-        return newAccount.getAccountNumber();
+        return accountNumber;
     }
 
     @Override
@@ -40,6 +33,39 @@ public class AccountServiceImpl implements AccountService{
         depositAccount.setBalance(newAmount);
         return newAmount;
     }
+
+    @Override
+    public long openSavingsAccount(Customer customer) throws MavenBankException {
+        if(customer == null) {
+            throw new MavenBankException("Customer or Type Can't be Null");
+        }
+        SavingsAccount newAccount = new SavingsAccount();
+        if(accountTypeExists(customer, newAccount.getClass().getTypeName())){
+            throw new MavenBankException("Account Type already Exists for this user, Thanks");
+        }
+        newAccount.setAccountNumber(BankService.generateAccountNumber());
+        customer.getAccounts().add(newAccount);
+
+        CustomerRepo.getCustomers().put(customer.getBVN(),customer);
+        return newAccount.getAccountNumber();
+    }
+
+    @Override
+    public long OpenCurrentAccount(Customer customer) throws MavenBankException {
+        if(customer == null) {
+            throw new MavenBankException("Customer Can't be Null");
+        }
+        CurrentAccount newAccount = new CurrentAccount();
+        if(accountTypeExists(customer, newAccount.getClass().getTypeName())){
+            throw new MavenBankException("Account Type already Exists for this user, Thanks");
+        }
+        newAccount.setAccountNumber(BankService.generateAccountNumber());
+        customer.getAccounts().add(newAccount);
+
+        CustomerRepo.getCustomers().put(customer.getBVN(),customer);
+        return newAccount.getAccountNumber();
+    }
+
     @Override
     public Account findAccount(Customer customer, long accountNumber)throws MavenBankException {
 
@@ -84,6 +110,12 @@ public class AccountServiceImpl implements AccountService{
     public void applyForOverdraft(Account account) {
         //TODO
     }
+
+    @Override
+    public LoanStatus applyForLoan() {
+     return null;
+    }
+
     public void checkForSufficientBalance(BigDecimal amount, Account account) throws MavenBankInsufficientAmountException {
         if(amount.compareTo(account.getBalance()) > 0){
             throw new MavenBankInsufficientAmountException("Insufficient account balance");
@@ -105,12 +137,12 @@ public class AccountServiceImpl implements AccountService{
 
     }
 
-    public boolean accountTypeExists(Customer customer, AccountType typeofAccount){
+    public boolean accountTypeExists(Customer customer, String type){
 
         boolean checkIfItExist = false;
         for (Account account:customer.getAccounts()) {
 
-            if (account.getTypeOfAccount().equals(typeofAccount)) {
+            if (account.getClass().getTypeName() == type) {
                 checkIfItExist = true;
                 break;
             }
