d430396b8121e69093c737a1c0cb33d6ba278aa4
akenz1901 <michael_aka1@icloud.com>
2021-08-18 22:40:08 +0100

    Savings Account Entity Created"

--- src/Entities/SavingsAccount.java
+++ src/Entities/SavingsAccount.java
@@ -1,2 +1,16 @@
-package Entities;public class SavingsAccount {
+package Entities;
+
+import java.math.BigDecimal;
+
+public class SavingsAccount extends Account{
+
+    public SavingsAccount(long accountNumber) {
+        setAccountNumber(accountNumber);
+    }
+    public SavingsAccount(long accountNumber,BigDecimal balance) {
+        setAccountNumber(accountNumber);
+        setBalance(balance);
+    }
+    public SavingsAccount(){
+    }
 }
