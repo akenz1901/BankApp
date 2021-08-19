0db8a85f1c1b5ceeb062030d50442f8f58799577
akenz1901 <michael_aka1@icloud.com>
2021-08-19 21:52:42 +0100

    Bank Service now helps us generate account Bank transaction history date and time

--- src/mavenBank/DataStore/services/BankService.java
+++ src/mavenBank/DataStore/services/BankService.java
@@ -1,8 +1,22 @@
 package mavenBank.DataStore.services;
 
+import java.time.Year;
+
 public class BankService {
     private static long currentBVN = 2;
     private static long currentAccountNumber = 1000110003;
+    private static long transactionId = 0;
+
+    public static long getTransactionId() {
+        return transactionId;
+    }
+    public static long generateId(){
+        transactionId++;
+        return transactionId;
+    }
+    private static void setTransactionId(long transactionId) {
+        BankService.transactionId = transactionId;
+    }
 
     private static void setCurrentBVN(long currentBVN) {
         BankService.currentBVN = currentBVN;
@@ -15,7 +29,6 @@ public class BankService {
     private static void setCurrentAccountNumber(long currentAccountNumber) {
         BankService.currentAccountNumber = currentAccountNumber;
     }
-
     public static long getCurrentBVN() {
         return currentBVN;
     }
