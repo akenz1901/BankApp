c3be00267482f858b17bd6878796ddb9cd6ea157
akenz1901 <michael_aka1@icloud.com>
2021-08-19 21:38:01 +0100

    Bank Transaction was created for Keeping track of transaction history which also triggered other object like bank transaction type and all

--- src/Entities/BankTransaction.java
+++ src/Entities/BankTransaction.java
@@ -1,2 +1,52 @@
-package Entities;public class BankTransaction {
+package Entities;
+
+import mavenBank.DataStore.BankTransactionType;
+import mavenBank.DataStore.services.BankService;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class BankTransaction {
+    private long id;
+    private LocalDateTime dateTime;
+    private BankTransactionType type;
+    private BigDecimal amount;
+
+    public BankTransaction(BankTransactionType type, BigDecimal tXAmount){
+        this.id = BankService.generateId();
+        this.dateTime = LocalDateTime.now();
+        this.type = type;
+        amount = tXAmount;
+    }
+    public long getTransactionId() {
+        return id;
+    }
+
+    public void setTransactionId(long transactionId) {
+        this.id = transactionId;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public BankTransactionType getType() {
+        return type;
+    }
+
+    public void setType(BankTransactionType type) {
+        this.type = type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
--- src/mavenBank/DataStore/BankTransactionType.java
+++ src/mavenBank/DataStore/BankTransactionType.java
@@ -1,23 +1,5 @@
 package mavenBank.DataStore;
 
-public enum TransactionServiceType {
-    DEPOSIT(), WITHDRAW();
-    private String withdrawalMessage;
-    private String depositMessage;
-
-    public String getWithdrawalMessage() {
-        return withdrawalMessage;
-    }
-
-    public void setWithdrawalMessage(String withdrawalMessage) {
-        this.withdrawalMessage = withdrawalMessage;
-    }
-
-    public String getDepositMessage() {
-        return depositMessage;
-    }
-
-    public void setDepositMessage(String depositMessage) {
-        this.depositMessage = depositMessage;
-    }
+public enum BankTransactionType {
+    DEPOSIT, WITHDRAW;
 }
