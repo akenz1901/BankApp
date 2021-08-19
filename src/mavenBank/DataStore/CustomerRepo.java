05dedb23573ddfd9fdf71d6e71a588d6df03899a
akenz1901 <michael_aka1@icloud.com>
2021-08-19 21:48:54 +0100

    Customer Repos which represents our database initially helps us create customer and customer account automatically and we included Bank Transaction in order to keep track of customer transactions

--- src/mavenBank/DataStore/CustomerRepo.java
+++ src/mavenBank/DataStore/CustomerRepo.java
@@ -1,8 +1,13 @@
 package mavenBank.DataStore;
 
 import Entities.*;
+import mavenBank.DataStore.services.AccountService;
+import mavenBank.DataStore.services.AccountServiceImpl;
+import mavenBank.DataStore.services.BankService;
+import mavenBank.Exceptions.MavenBankException;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,10 +38,28 @@ public class CustomerRepo {
         joshua.setPhone("08183563309");
         joshua.setPassword("1248489284749");
         Account joshuaSavingsAccount = new SavingsAccount(1000110001);
-        joshuaSavingsAccount.setPin("1111");
+        joshua.setRelationshipStartDate(joshuaSavingsAccount.getStartDate());
+        BankTransaction initialDeposit = new BankTransaction(BankTransactionType.DEPOSIT, BigDecimal.valueOf(300000));
+        joshuaSavingsAccount.getTransactions().add(initialDeposit);
+
+        BankTransaction upkeepAllowanceDeposit= new BankTransaction(BankTransactionType.DEPOSIT, BigDecimal.valueOf(50000));
+        upkeepAllowanceDeposit.setDateTime(LocalDateTime.now().minusMonths(3));
+        joshuaSavingsAccount.getTransactions().add(upkeepAllowanceDeposit);
+
+        BankTransaction mayAllowanceDeposit= new BankTransaction(BankTransactionType.DEPOSIT, BigDecimal.valueOf(50000));
+        mayAllowanceDeposit.setDateTime(LocalDateTime.now().minusMonths(2));
+        joshuaSavingsAccount.getTransactions().add(mayAllowanceDeposit);
+
+        BankTransaction juneAllowanceDeposit = new BankTransaction(BankTransactionType.DEPOSIT, BigDecimal.valueOf(50000));
+        juneAllowanceDeposit.setDateTime(LocalDateTime.now().minusMonths(2));
+        joshuaSavingsAccount.getTransactions().add(juneAllowanceDeposit);
+
+        joshuaSavingsAccount.setBalance(BigDecimal.valueOf(450000));
+
         joshua.getAccounts().add(joshuaSavingsAccount);
 
-        Account joshuaCurrentAccount = new CurrentAccount(1000110002,  new BigDecimal(500000000));
+
+        Account joshuaCurrentAccount = new CurrentAccount(1000110002,  new BigDecimal(50000000));
         joshua.getAccounts().add(joshuaCurrentAccount);
         customers.put(joshua.getBVN(), joshua);
 
@@ -48,7 +71,8 @@ public class CustomerRepo {
         judas.setPhone("08183563309");
         judas.setPassword("1248489284749");
         Account judasSavingsAccount = new SavingsAccount(1000110003);
-        judasSavingsAccount.setPin("1234");
+
+        judas.setRelationshipStartDate(judasSavingsAccount.getStartDate());
         judas.getAccounts().add(judasSavingsAccount);
         customers.put(judas.getBVN(), judas);
 
