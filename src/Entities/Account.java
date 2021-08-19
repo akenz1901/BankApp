76f7f54dada419f3f50986bb1656e99f59de7dc0
akenz1901 <michael_aka1@icloud.com>
2021-08-19 21:35:28 +0100

    Account Object now being changed to Abstracted and Added Bank Transaction with some instance methods

--- src/Entities/Account.java
+++ src/Entities/Account.java
@@ -1,19 +1,39 @@
 package Entities;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class Account {
     private long accountNumber;
     private String pin;
     private BigDecimal balance = BigDecimal.ZERO;
-    private Loan accountLoan;
-
-    public Loan getAccountLoan() {
-        return accountLoan;
+    private LoanRequest accountLoanRequest;
+    private LocalDateTime startDate;
+    private Set<BankTransaction> transactions = new HashSet<>();
+    public LocalDateTime getStartDate() {
+        return startDate;
     }
 
-    public void setAccountLoan(Loan accountLoan) {
-        this.accountLoan = accountLoan;
+    public Set<BankTransaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(Set<BankTransaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LoanRequest getAccountLoanRequest() {
+        return accountLoanRequest;
+    }
+
+    public void setAccountLoanRequest(LoanRequest accountLoanRequest) {
+        this.accountLoanRequest = accountLoanRequest;
     }
 
     public long getAccountNumber() {
