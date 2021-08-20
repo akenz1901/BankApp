ce32d5bc0e374bef6c7d19d509fc604dda386467
akenz1901 <michael_aka1@icloud.com>
2021-08-20 23:34:31 +0100

    Interface Object was created to implement Stretegy pattern which prompted Loan Engine by Balance

--- src/mavenBank/engines/LoanEngine.java
+++ src/mavenBank/engines/LoanEngine.java
@@ -1,2 +1,26 @@
-package mavenBank.engines;public interface LoanEngine {
+package mavenBank.engines;
+
+import Entities.Account;
+import Entities.Customer;
+import mavenBank.Exceptions.MavenBankLoanException;
+
+import java.math.BigDecimal;
+
+public interface LoanEngine {
+
+    BigDecimal calculateAmountAutoApprove(Customer customer, Account accountSeekingLoan) throws MavenBankLoanException;
+
+    default void validateLoanRequest(Customer customer, Account accountSeekingLoan) throws MavenBankLoanException {
+        if (customer == null) {
+            throw new MavenBankLoanException("No Customer provided for loan request");
+        }
+        validateLoanRequest(accountSeekingLoan);
+    }
+    default void validateLoanRequest(Account accountSeekingLoan)throws MavenBankLoanException{
+        if(accountSeekingLoan == null){
+            throw new MavenBankLoanException("An account is required for processing");
+        }if(accountSeekingLoan.getAccountLoanRequest() == null){
+            throw new MavenBankLoanException("No Loan provided for processing");
+        }
+    }
 }
--- src/mavenBank/engines/LoanEngineByBalance.java
+++ src/mavenBank/engines/LoanEngineByBalance.java
@@ -1,2 +1,29 @@
-package mavenBank.engines;public class LoanEngineByBalance {
+package mavenBank.engines;
+
+import Entities.Account;
+import Entities.Customer;
+import mavenBank.DataStore.LoanRequestStatus;
+import mavenBank.Exceptions.MavenBankLoanException;
+
+import java.math.BigDecimal;
+
+public class LoanEngineByBalance implements LoanEngine {
+    @Override
+    public BigDecimal calculateAmountAutoApprove(Customer customer, Account account) throws MavenBankLoanException {
+        validateLoanRequest(customer, account);
+        BigDecimal totalVolumeBalance = BigDecimal.valueOf(0.2);
+
+        BigDecimal totalCustomerBalance = BigDecimal.ZERO;
+        if (customer.getAccounts().size() > BigDecimal.ONE.intValue()) {
+            for (Account customerAccount : customer.getAccounts()) {
+                totalCustomerBalance = totalCustomerBalance.add(customerAccount.getBalance());
+            }
+        }
+        BigDecimal loanAmountApproveAutomatically = BigDecimal.ZERO;
+        if (totalCustomerBalance.intValue() > BigDecimal.ZERO.intValue()){
+            loanAmountApproveAutomatically = totalCustomerBalance.multiply(totalVolumeBalance);
+        }
+
+        return loanAmountApproveAutomatically;
+    }
 }
